4e6c40b7fd8ac69d1f6aad7886619cb241a9e8c8
Robert Eaglestone <robert.eaglestone@gmail.com>
2021-06-11 18:02:19 -0500

    CHORE: adding test coverage and organizing the test data...

--- main.c
+++ main.c
@@ -14,9 +14,19 @@ void resetTestArena()
     clearArena();
 
     // set up some test data 
-    loadCell("hcf #7   #0", 4049);
+    loadCell("hcf #1   #9", 4041);
+    loadCell("hcf #2   #8", 4042);
+    loadCell("hcf #3   #7", 4043);
+    loadCell("hcf #4   #6", 4044);
+    loadCell("hcf #5   #5", 4045);
+    loadCell("hcf #6   #4", 4046);
+    loadCell("hcf #7   #3", 4047);
+    loadCell("hcf #8   #2", 4048);
+    loadCell("hcf #9   #1", 4049);
     loadCell("hcf #17 #19", 4050);
     loadCell("hcf @-1  #7", 4051);
+    loadCell("hcf #5   #5", 4052);
+    loadCell("hcf #6  #22", 4053);
 }
 
 void main() //int argc, char* argv[])
@@ -44,8 +54,12 @@ void main() //int argc, char* argv[])
     resetTestArena();
     RUN_TEST("t/sub  a  b", 4040, "sub 9 10",  4050, "hcf #17 #12");   // assumes test data
 
-
 // These are works in progress
+
+    RUN_TEST("t/mul #a  b", 4045, "mul #5 7",  4052, "hcf #5  #25");   // assumes test data
+    RUN_TEST("t/div @a  b", 4045, "div @3 8",  4053, "hcf #6  #4");    // assumes test data
+
+
 
 
 }
